4e174db4edd502be71cba17f49d97bcfebd22df0
Gurvan Renault <renault.gurvan@gmail.com>
2017-07-20 19:54:45 +0200

    [ORIGINAL] Stars_in_your_eyes by Pawn

---
+++ Pawn/stars_in_your_eyes.py
@@ -0,0 +1,30 @@
+
+#ORIGINAL
+# Author : PoetryOfCoding/Pawn
+# Title : Stars in your eyes
+print "You are : ";
+print "1.A man ";
+print "2.A  woman ";
+answer=input()
+gender="person";
+char="She";
+if answer==1:
+    gender="man";
+if gender==2:
+    answer="woman";
+    char="He";
+
+try :
+       lookLikeCoolAndFunny(me);
+except:
+
+        print "You take a look  at this "+gender;
+your_eyes=(0,0);
+stars="*";
+if (stars in your_eyes):
+    print("There is stars in your eyes ");
+else:
+    print("There is no stars in your eyes");
+my_eyes_when_I_see_you=("*","*")
+if (stars in my_eyes_when_I_see_you):
+    print("But there are in mine when I  see you everyday");
